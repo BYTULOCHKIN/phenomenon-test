7d499b9ce5e4f874710a5490ba63dd2f44725c1b
Ruslan Krasiuk <ruslan.krasuk@appsdelivered.com>
2023-09-10 19:58:33 +0300

    fixed popup

--- src/components/users-table/ColumnsPopup.tsx
+++ src/components/users-table/ColumnsPopup.tsx
@@ -18,11 +18,12 @@ const PopoverDemo = () => {
       </Popover.Trigger>
       <Popover.Portal className={'relative'}>
         <Popover.Content
+          sticky="always"
           className="relative top-4 right-12 rounded-xl p-5 w-[260px] bg-white shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2)] focus:shadow-[0_10px_38px_-10px_hsla(206,22%,7%,.35),0_10px_20px_-15px_hsla(206,22%,7%,.2),0_0_0_2px_theme(colors.violet7)] will-change-[transform,opacity] data-[state=open]:data-[side=top]:animate-slideDownAndFade data-[state=open]:data-[side=right]:animate-slideLeftAndFade data-[state=open]:data-[side=bottom]:animate-slideUpAndFade data-[state=open]:data-[side=left]:animate-slideRightAndFade"
           sideOffset={5}
         >
           <SearchBar value={searchValue} setValue={setSearchValue} />
-          <div className="flex flex-col gap-2.5">
+          <div className="w-full flex flex-col gap-2.5">
             {columns
               .filter((c) => c.displayName.toLowerCase().includes(searchValue.trim().toLowerCase()))
               .map((column) => (
